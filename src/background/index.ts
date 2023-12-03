60fd60c0793917a5b43e3e908b89cb7812cabf5d
Jeff Prouty <jeff.prouty@gmail.com>
2023-12-03 07:34:01 -0800

    Initial scaffold for takeout ext.

---
+++ src/background/index.ts
@@ -0,0 +1,9 @@
+console.log('background is running 3')
+
+
+
+chrome.runtime.onMessage.addListener((request) => {
+  if (request.type === 'COUNT') {
+    console.log('background has received a message from popup, and count is ', request?.count)
+  }
+})
